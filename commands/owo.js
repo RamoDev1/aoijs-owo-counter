b2fcd46bfad399e41cbd429ecd68caa986f1629d
Ramodev88 <126801509+Ramodev88@users.noreply.github.com>
2023-08-07 14:51:30 +0300

    Update owo.js

--- commands/owo.js
+++ commands/owo.js
@@ -1,1 +1,47 @@
-
+module.exports = {
+name: "$alwaysExecute",
+$if: "old",
+code: `
+$if[$message==owo]
+$setGlobalUserVar[totalowo;$sum[$getGlobalUserVar[totalowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[monthlyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[weeklyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[dailyowo];1]]
+$globalUserCooldown[11s;]
+$endif
+$if[$message==Owo]
+$setGlobalUserVar[totalowo;$sum[$getGlobalUserVar[totalowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[monthlyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[weeklyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[dailyowo];1]]
+$globalUserCooldown[11s;]
+$endif
+$if[$message==OwO]
+$setGlobalUserVar[totalowo;$sum[$getGlobalUserVar[totalowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[monthlyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[weeklyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[dailyowo];1]]
+$globalUserCooldown[11s;]
+$endif
+$if[$message==oWo]
+$setGlobalUserVar[totalowo;$sum[$getGlobalUserVar[totalowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[monthlyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[weeklyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[dailyowo];1]]
+$globalUserCooldown[11s;]
+$endif
+$if[$message==owO]
+$setGlobalUserVar[totalowo;$sum[$getGlobalUserVar[totalowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[monthlyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[weeklyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[dailyowo];1]]
+$globalUserCooldown[11s;]
+$endif
+$if[$message==OWO]
+$setGlobalUserVar[totalowo;$sum[$getGlobalUserVar[totalowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[monthlyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[weeklyowo];1]]
+$setGlobalUserVar[monthlyowo;$sum[$getGlobalUserVar[dailyowo];1]]
+$globalUserCooldown[11s;]
+$endif
+`
