c2a306341b424b9c0f1b1b0fc8f4ed1daadd79a6
Ramodev88 <126801509+Ramodev88@users.noreply.github.com>
2023-08-07 15:30:55 +0300

    Create check.js

---
+++ commands/check.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "stat",
+  code: `
+  $title[1;OwO]
+  $description[1;**Today**\n> '$getGlobalUserVar[dailyowo]'\n**Weekly**\n> '$getGlobalUserVar[weeklyowo]'\n**Monthly**\n> '$getGlobalUserVar[monthlyowo]']
+  $footer[$username[$authorID]'s Stats]
+  $color[1;Random]`
+}
